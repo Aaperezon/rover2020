8c87882be11d6748b358d0e500fac90070f3fcc4
Aaron Perez Ontiveros <aaron-po@hotmail.co.uk>
2019-12-16 23:47:44 -0600

    IMU addition

--- forma.js
+++ forma.js
@@ -4,20 +4,26 @@ var intX = 1800;
 var intY = 850;
 var screen;
 if (canvas.getContext){
+  alert("Hello! I am an alert box!!");
+
  // dibuja con tú código aquí
   screen = canvas.getContext('2d');
   screen.fillStyle = "#4E597B";
   screen.fillRect(0,0,intX,intY);
-
-
+  screen.alert("Hola");
   Thermometer(25,0);
+  IMUPitch(1700,200);
+  IMUYaw(1700,320);
+  IMURoll(1700,440);
 
 } else {
  alert("Su navegador no soporta canvas :O");
 }
 
 
+
 function Thermometer(posX, posY){
+
   var thermometer = new Image;
   var measure = 40;
   thermometer.src = 'thermometer.jpg';
@@ -30,4 +36,29 @@ function Thermometer(posX, posY){
   screen.rect(posX-25,posY+measure+62,140,0);
   screen.stroke();
 
+}
+
+function IMUPitch(posX, posY){
+  var pitch = new Image;
+  // var measure = 40;
+  pitch.src = 'pitch.jpg';
+  pitch.onload = function(){
+    screen.drawImage(pitch, posX, posY, 80, 350);
+  }
+}
+function IMUYaw(posX, posY){
+  var yaw = new Image;
+  // var measure = 40;
+  yaw.src = 'yaw.jpg';
+  yaw.onload = function(){
+    screen.drawImage(yaw, posX, posY, 80, 350);
+  }
+}
+function IMURoll(posX, posY){
+  var roll = new Image;
+  // var measure = 40;
+  roll.src = 'roll.jpg';
+  roll.onload = function(){
+    screen.drawImage(roll, posX, posY, 80, 350);
+  }
 }
--- rover2020.php
+++ rover2020.php
@@ -1,6 +1,11 @@
 <?php
     $conexion = mysqli_connect('localhost','root','','rover2020');
     
+    $_POST['str']="Esto esta en php";
+    echo "<script>\n";
+    echo "strPjs='".$_POST['str']."'\n";
+    echo "</script>\n";
+    
 ?>
 
 <!DOCTYPE html>
@@ -45,33 +50,47 @@
             $result=mysqli_query($conexion,$sql);
             while($mostrar = mysqli_fetch_array($result)){
             ?>
+                <?php $timeVar = $mostrar['time'] ?>
+                <?php $temperatureVar = $mostrar['temperature'] ?>
+                <?php $humidityVar = $mostrar['humidity'] ?>
+                <?php $flex_frVar = $mostrar['flex_fr'] ?>
+                <?php $flex_flVar = $mostrar['flex_fl'] ?>
+                <?php $flex_rrVar = $mostrar['flex_rr'] ?>
+                <?php $flex_rlVar = $mostrar['flex_rl'] ?>
+                <?php $gyro_xVar = $mostrar['gyro_x'] ?>
+                <?php $gyro_yVar = $mostrar['gyro_y'] ?>
+                <?php $gyro_zVar = $mostrar['gyro_z'] ?>
+                <?php $lightVar = $mostrar['light'] ?>
                 <tr>
-                    <td><?php echo $mostrar['time'] ?></td>
+                    <td><?php echo $timeVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['temperature'] ?></td>
+                    <td><?php echo $temperatureVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['humidity'] ?></td>
+                    <td><?php echo $humidityVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['flex_fr'] ?></td>
+                    <td><?php echo $flex_frVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['flex_fl'] ?></td>
+                    <td><?php echo $flex_flVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['flex_rr'] ?></td>
+                    <td><?php echo $flex_rrVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['flex_rl'] ?></td>
+                    <td><?php echo $flex_rlVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['gyro_x'] ?></td>
+                    <td><?php echo $gyro_xVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['gyro_y'] ?></td>
+                    <td><?php echo $gyro_yVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['gyro_z'] ?></td>
+                    <td><?php echo $gyro_zVar?></td>
                     <td>  </td>
-                    <td><?php echo $mostrar['light'] ?></td>
+                    <td><?php echo $lightVar?></td>
                     <td>  </td>
                 </tr>
             <?php
             }
             ?>
+
+
+           
         </table>
     </body>
 
