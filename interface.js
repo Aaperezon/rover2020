d1edf1230a3257cb05f11d0a5029db03dfbbf36a
Aaron Perez Ontiveros <aaron-po@hotmail.co.uk>
2019-12-19 12:20:03 -0600

    Beckground for IMU images

--- interface.js
+++ interface.js
@@ -7,13 +7,13 @@ if (canvas.getContext){
 
  // dibuja con tú código aquí
   screen = canvas.getContext('2d');
-  screen.fillStyle = "#4E597B";
+  screen.fillStyle = "#FFDA73";
   screen.fillRect(0,0,intX,intY);
   Thermometer(30,10);
-  IMUPitch(1200,100);
-  IMURoll(1200,200);
-  IMUYaw(1200,300);
-
+  IMUPitch(800,600);
+  IMUYaw(1100,600);
+  IMURoll(1400,600);
+ 
   
 
 } else {
@@ -34,24 +34,43 @@ function Thermometer(posX, posY){
 function IMUPitch(posX, posY){
   var pitch = new Image;
   // var measure = 40;
-  pitch.src = 'pitch.jpg';
+  pitch.src = 'pitch.png';
+
+  var back = new Image;
+  back.src = 'IMUBackground.png';
+
   pitch.onload = function(){
-    screen.drawImage(pitch, posX, posY);
+    screen.drawImage(pitch, posX+10, posY+85,215,55);
+    screen.drawImage(back, posX, posY,240,240);
   }
+ 
 }
 function IMUYaw(posX, posY){
   var yaw = new Image;
   // var measure = 40;
-  yaw.src = 'yaw.jpg';
+  yaw.src = 'yaw.png';
+
+  var back = new Image;
+  back.src = 'IMUBackground.png';
+
   yaw.onload = function(){
-    screen.drawImage(yaw, posX, posY);
+    //rotate(10 * Math.PI / 180);
+    screen.drawImage(yaw, posX+67, posY+23,105,190);
+    screen.drawImage(back, posX, posY,240,240);
+
   }
 }
 function IMURoll(posX, posY){
   var roll = new Image;
   // var measure = 40;
-  roll.src = 'roll.jpg';
+  roll.src = 'roll.png';
+
+  var back = new Image;
+  back.src = 'IMUBackground.png';
+  
   roll.onload = function(){
-    screen.drawImage(roll, posX, posY);
+    screen.drawImage(roll, posX+29, posY+70,180,90);
+    screen.drawImage(back, posX, posY,240,240);
+
   }
 }
